eb82fa032aa61c667cba533370070b4d9e764b3a
UMANG AGARWAL <39197141+UMANG380@users.noreply.github.com>
2018-06-07 10:43:11 +0530

    Update staircase.py

--- staircase.py
+++ staircase.py
@@ -1,9 +1,10 @@
 n=int(input())
 def staircase(n):
-    if(n==0 or n==1):
-      return 1;
-    elif(n==2):
-      return 2
+
+    if(n<=2):
+      return n
+    elif n==3:
+      return 4
     else:
       return staircase(n-1)+staircase(n-2)+staircase(n-3)
 
